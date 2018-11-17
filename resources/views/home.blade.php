3dc1f5037417f3942450fa6dce31248e38274e52
GrCOTE7 <GrCOTE7@GMail.com>
2018-11-17 18:52:27 +0100

    Nett

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                     </div>
                     @endif
 
-                    You are logged in!<br><br>
+                    You are logged in!
                 </div>
             </div>
         </div>
